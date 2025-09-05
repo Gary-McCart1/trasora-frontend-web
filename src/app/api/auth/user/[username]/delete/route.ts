ad4fb7764ca6f047af7c03fb3052bbdfe0af69ae
Gary McCart <gwmccart3@gmail.com>
2025-09-05 00:59:52 -0400

    change delete request type

--- src/app/api/auth/user/[username]/delete/route.ts
+++ src/app/api/auth/user/[username]/delete/route.ts
@@ -1,14 +1,14 @@
-import { NextResponse, NextRequest } from "next/server";
+// src/app/api/auth/user/[username]/delete/route.ts
+import { NextResponse } from "next/server";
 
 const BASE_URL =
   process.env.BACKEND_URL || "https://trasora-backend-e03193d24a86.herokuapp.com";
 
-export async function DELETE(
-  req: NextRequest,
-  { params }: { params: { username: string } }
-) {
+export async function DELETE(req: Request, { params }: { params: { username: string } }) {
+  const username = params.username;
+
   try {
-    const res = await fetch(`${BASE_URL}/api/auth/user/${params.username}`, {
+    const res = await fetch(`${BASE_URL}/api/auth/user/${username}`, {
       method: "DELETE",
       credentials: "include",
     });
