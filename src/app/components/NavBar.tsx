63a51dcd48ab3a7a68d90f147cef5fa75e641990
Gary McCart <gwmccart3@gmail.com>
2025-09-22 14:54:38 -0400

    add more nav vertical spacing for mobile

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -109,7 +109,7 @@ export default function Navbar() {
   }
 
   return (
-    <nav className="bg-zinc-950 text-white shadow-md px-4 py-2 sticky top-0 z-50">
+    <nav className="bg-zinc-950 text-white shadow-md px-4 py-5 md:py-2 sticky top-0 z-50">
       <div className="flex items-center justify-between">
         {/* Logo */}
         <div className="flex-shrink-0 z-10">
