3f6749617d4a7fac79d96db80c2a9c31a5909f93
Gary McCart <gwmccart3@gmail.com>
2025-09-25 14:02:34 -0400

    make nav logo smaller

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -86,7 +86,7 @@ export default function Navbar() {
 
   if (loading) {
     return (
-      <nav className="bg-zinc-950 text-white  px-4 py-4 mt-2">
+      <nav className="bg-zinc-950 text-white  px-4 pt-4 mt-2">
         <div className="flex items-center justify-between">
           <Image
             src="/trasora.png"
