687bbd7946ddd711490020df0cc21d6b6069efcc
Gary McCart <gwmccart3@gmail.com>
2025-10-06 13:33:39 -0400

    terms error

--- src/app/terms-of-use/page.tsx
+++ src/app/terms-of-use/page.tsx
@@ -1,6 +1,13 @@
-// This can remain a server component
+import { Suspense } from 'react'; // 1. Import Suspense
 import TermsOfUseClient from './TermsOfUseClient';
 
+export const dynamic = 'force-dynamic';
+
 export default function TermsOfUsePage() {
-  return <TermsOfUseClient />;
+  return (
+    
+    <Suspense fallback={<div>Loading...</div>}> 
+      <TermsOfUseClient />
+    </Suspense>
+  );
 }
