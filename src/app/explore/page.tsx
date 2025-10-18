1feac8cc2bfbdbf15707af40b05f94526810338a
Gary McCart <gwmccart3@gmail.com>
2025-10-18 00:15:28 -0400

    remove search in explore page on xl devices

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -77,7 +77,7 @@ export default function ExplorePage() {
 
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-12 py-4">
-      <div className="mb-[5rem]">
+      <div className="mb-[5rem] xl:hidden">
         <SearchBar / >
       </div>
       <h1 className="text-4xl sm:text-5xl font-extrabold mb-4 text-center text-white drop-shadow-lg leading-tight">
