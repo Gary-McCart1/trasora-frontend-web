fa72963bdcb50c6234c0ac73362e11ec42f26c8c
Gary McCart <gwmccart3@gmail.com>
2026-01-12 23:33:02 -0500

    5rem padding

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -146,7 +146,7 @@ export default function Navbar() {
       className="
     bg-zinc-950 text-white shadow-md px-4
     fixed top-0 left-0 right-0 z-50
-    pt-[env(safe-area-inset-top)]
+    pt-[5rem]
   "
     >
       <div className="flex items-center justify-between">
