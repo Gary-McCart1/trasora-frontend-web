217e5c2b828c72f846d567e338f98ba91c8ffe2f
Gary McCart <gwmccart3@gmail.com>
2026-01-12 23:29:51 -0500

    add margin top

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -144,6 +144,7 @@ export default function Navbar() {
   return (
     <nav
       className="
+      mt-5
     bg-zinc-950 text-white shadow-md px-4
     fixed top-0 left-0 right-0 z-50
     pt-[env(safe-area-inset-top)]
